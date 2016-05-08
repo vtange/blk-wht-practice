435ca8045838c72e28e2a0b247422e861e22bc06
Victor Tang <victortang1992@gmail.com>
2016-05-08 14:44:33 -0700

    DRY and reset cookies per test

--- Selenium-based/spec.js
+++ Selenium-based/spec.js
@@ -1,36 +1,55 @@
 var assert = require('assert');
 var selenium = require('selenium-webdriver');
 var test = require('selenium-webdriver/testing');
-
+var driver;
+ 
 const timeOut = 15000;
+ 
+/*
+//Open Firefox and get website before each test
+test.beforeEach(function() {
+  this.timeout(mochaTimeOut);
+  driver = new selenium.Builder().
+      withCapabilities(selenium.Capabilities.firefox()).
+      build();
+  driver.get("https://decohere.herokuapp.com/planets");
+});
+
+// Close Firefox after each test 
+test.afterEach(function() {
+  driver.quit();
+});
+*/
+
+//Open Firefox and get website before test
+ test.before(function() {
+  this.timeout(timeOut);
+  driver = new selenium.Builder().
+      withCapabilities(selenium.Capabilities.firefox()).
+      build();
+  driver.get("https://decohere.herokuapp.com/planets");
+});
+
+//Close Firefox and get website before test
+test.after(function() {
+  driver.quit();
+});
+
+//Clear Firefox cookies after each test -> Force new session
+test.afterEach(function() {
+  driver.manage().deleteAllCookies();
+});
 
 test.describe('calculating weights', function() {
-  this.timeout(timeOut);
   test.it('calculates weights', function() {
-    var driver = new selenium.Builder().
-        withCapabilities(selenium.Capabilities.firefox()).
-        build();
-
-    driver.get("https://decohere.herokuapp.com/planets");
-
     driver.isElementPresent(selenium.By.id('wt')).then(function(weight) {
       assert.equal(weight, true, "Weight entry not possible");
     });
-
-    driver.quit();
   });
-
-test.it.only('provides no default weight', function() {
-    var driver = new selenium.Builder().
-        withCapabilities(selenium.Capabilities.firefox()).
-        build();
-
-    driver.get("https://decohere.herokuapp.com/planets");
-
+ 
+  test.it('provides no default weight', function() {
     driver.findElement(selenium.By.id('wt')).getText().then(function(weight) {
       assert.equal(weight, '', "Weight started with values");
     });
-
-    driver.quit();
   });
 });
