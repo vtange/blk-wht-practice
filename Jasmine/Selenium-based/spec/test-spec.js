ff1c5efce3eba48c88bdd9b51ae31e8a0eacd875
Victor Tang <victortang1992@gmail.com>
2016-05-08 22:06:48 -0700

    turn on firefox

--- Jasmine/Selenium-based/spec/test-spec.js
+++ Jasmine/Selenium-based/spec/test-spec.js
@@ -1,4 +1,15 @@
 var selenium = require('selenium-webdriver');
 
 describe('calculating weights', function() {
+  beforeEach(function(done) {
+    this.driver = new selenium.Builder().
+      withCapabilities(selenium.Capabilities.firefox()).
+      build();
+
+    this.driver.get('http://dialogic.herokuapp.com/weight').then(done);
+  });
+
+  afterEach(function(done) {
+    this.driver.quit().then(done);
+  });
 });
