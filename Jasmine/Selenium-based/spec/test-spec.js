ce6fede446260da6df1d04281d18350425c7df4b
Victor Tang <victortang1992@gmail.com>
2016-05-09 12:07:28 -0700

    change default time interval to avoid error. organize test

--- Jasmine/Selenium-based/spec/test-spec.js
+++ Jasmine/Selenium-based/spec/test-spec.js
@@ -1,41 +1,49 @@
 var selenium = require('selenium-webdriver');
 var chromedriver = require('chromedriver');
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 12000;
 
-describe('calculating weights', function() {
-  beforeEach(function(done) {
-    this.driver = new selenium.Builder().
-      withCapabilities(selenium.Capabilities.chrome()).
-      build();
-
-    this.driver.get('http://dialogic.herokuapp.com/weight').then(done);
-  });
-
-  afterEach(function(done) {
-    this.driver.quit().then(done);
-  });
+describe('test tutorial', function() {
 	
-	
-  it('should be on the weight page', function(done) {
-    var element = this.driver.findElement(selenium.By.tagName('article'));
- 
-    element.getAttribute('id').then(function(id) {
-      expect(id).toBe('weight');
-      done();
-    });
-  }, 12000);
+	// Open Chrome and get website before each test
+	  beforeEach(function(done) {
+		this.driver = new selenium.Builder().
+		  withCapabilities(selenium.Capabilities.chrome()).
+		  build();
 
-  it('will calculate a weight of 200', function(done) {
-    var myWeight = this.driver.findElement(selenium.By.id('wt'));
-    myWeight.sendKeys('200');
- 
-    var calculate = this.driver.findElement(selenium.By.id('calculate'));
-    calculate.click();
- 
-    var mercuryWeight = this.driver.findElement(selenium.By.id('outputmrc'));
-    mercuryWeight.getAttribute('value').then(function(value) {
-      expect(value).toBe('75.6');
-      done();
-    });
-  });
+		this.driver.get('http://dialogic.herokuapp.com/weight').then(done);
+	  });
 
+	// Close Chrome after each test
+	  afterEach(function(done) {
+		this.driver.quit().then(done);
+	  });
+
+	describe('calculating weights', function() {
+
+	// check page contents with url
+	  it('should be on the weight page', function(done) {
+		var element = this.driver.findElement(selenium.By.tagName('article'));
+
+		element.getAttribute('id').then(function(id) {
+		  expect(id).toBe('weight');
+		  done();
+		});
+	  });
+
+	// interact with webpage and confirm calculation
+	  it('will calculate a weight of 200', function(done) {
+		var myWeight = this.driver.findElement(selenium.By.id('wt'));
+		myWeight.sendKeys('200');
+
+		var calculate = this.driver.findElement(selenium.By.id('calculate'));
+		calculate.click();
+
+		var mercuryWeight = this.driver.findElement(selenium.By.id('outputmrc'));
+		mercuryWeight.getAttribute('value').then(function(value) {
+		  expect(value).toBe('75.6');
+		  done();
+		});
+	  });
+
+	});
 });
