a68b06f0a578297b8ebe8f4bb8c3c15048b3d463
Victor Tang <victortang1992@gmail.com>
2016-05-10 16:30:58 -0700

    custom firefox install pointer

--- Mocha/Selenium-based/try.js
+++ Mocha/Selenium-based/try.js
@@ -1,4 +1,5 @@
-var selenium = require('selenium-webdriver'); 
-var driver = new selenium.Builder()
-     .forBrowser('firefox')
-     .build(); driver.get('http://iqdb.org/');
+var firefox = require('selenium-webdriver/firefox');
+var binary = new firefox.Binary('C:/Portable/FirefoxPortable/FirefoxPortable.exe');
+var options = new firefox.Options().setBinary(binary);
+var driver = new firefox.Driver(options);
+driver.get('http://iqdb.org/');
