569da17f55277577cc2cbe87ef2fa85a8d3dc965
Victor Tang <victortang1992@gmail.com>
2016-05-10 18:47:44 -0700

    installed, try phantomjs.exe in root folder

---
+++ Mocha/Selenium-based/phantom.js
@@ -0,0 +1,5 @@
+var selenium = require('selenium-webdriver'); 
+var driver = new selenium.Builder()
+     .forBrowser('phantomjs')
+     .build(); driver.get('http://iqdb.org/');
+driver.get('http://iqdb.org/');
