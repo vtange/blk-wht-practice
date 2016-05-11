ba33e7de9751453d33eda43a4bb2cb770a61599e
Victor Tang <victortang1992@gmail.com>
2016-05-10 21:49:59 -0700

    use phantomjs-prebuilt module

--- Mocha/Selenium-based/phantom.js
+++ Mocha/Selenium-based/phantom.js
@@ -1,5 +1,14 @@
-var selenium = require('selenium-webdriver'); 
-var driver = new selenium.Builder()
-     .forBrowser('phantomjs')
-     .build(); driver.get('http://iqdb.org/');
-driver.get('http://iqdb.org/');
+var path = require('path');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs-prebuilt');
+var binPath = phantomjs.path;
+ 
+var childArgs = [
+  path.join(__dirname, 'loadspeed.js'),
+  'http://www.vtange.net'
+]
+ 
+childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+  // handle results 
+	console.log(stdout);
+})
