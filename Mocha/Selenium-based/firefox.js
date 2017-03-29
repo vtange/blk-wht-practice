9f5f2ae694f8e0f0ba7773b7b6a2b86f71cddbdf
Victor Tang <victortang1992@gmail.com>
2017-03-28 20:08:31 -0700

    firefox update

--- Mocha/Selenium-based/firefox.js
+++ Mocha/Selenium-based/firefox.js
@@ -1,7 +1,10 @@
 //custom-Firefox-location driver
+//portable ff doesn't work, native ff will always conflict with existing ffport.
 var firefox = require('selenium-webdriver/firefox');
-var binary = new firefox.Binary('C:/Portable/FirefoxPortable/FirefoxPortable.exe');
-var options = new firefox.Options().setBinary(binary);
+var binary = new firefox.Binary('C:/Portable/FirefoxForTesting/FirefoxPortable.exe');
+var options = new firefox.Options().setBinary(binary).useMarionette(true); //marionette req'd for selenium 2.53+
 var driver = new firefox.Driver(options);
 
 exports = module.exports = driver;
+
+//USE CHROME 03/28/17-->
